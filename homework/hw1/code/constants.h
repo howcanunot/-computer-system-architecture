b7391102f101d3b7e2ee1ab0617917b4643ce1e6
howcanunot <kernbldhf@gmail.com>
2021-10-02 20:51:46 +0300

    общие константы

--- homework/hw1/code/constants.h
+++ homework/hw1/code/constants.h
@@ -5,6 +5,6 @@
 #ifndef CSA_HWS_CONSTANTS_H
 #define CSA_HWS_CONSTANTS_H
 
-const size_t MAX_NAME_LEN = 30;
+const size_t MAX_NAME_LEN = 40;
 const size_t MIN_YEAR = 1950;
 #endif //CSA_HWS_CONSTANTS_H
