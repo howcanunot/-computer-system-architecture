d19ef2d019bc41c08d1272e8c0ea9fe9d0dc6930
howcanunot <kernbldhf@gmail.com>
2021-10-03 22:40:26 +0300

    fixed tests

--- homework/run_all_tests.sh
+++ homework/run_all_tests.sh
@@ -1,21 +1,21 @@
 echo "\nTest #1";
-time C:\Users\kernb\source\repos\computer-system-architecture\homework\hw1\code\cmake-build-debug -f /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/input1.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output1_1.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output1_2.txt;
+time /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/code/cmake-build-debug/hw1 -f /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/test01.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/out01_1.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/out01_2.txt;
 
 echo "\nTest #2";
-time /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/code/cmake-build-debug/hw1 -f /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/input2.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output2_1.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output2_2.txt;
+time /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/code/cmake-build-debug/hw1 -f /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/test02.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/out02_1.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/out02_2.txt;
 
 echo "\nTest #3";
-time /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/code/cmake-build-debug/hw1 -f /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/input3.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output3_1.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output3_2.txt;
+time /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/code/cmake-build-debug/hw1 -f /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/test03.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/out03_1.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/out03_2.txt;
 
 echo "\nTest #4";
-time /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/code/cmake-build-debug/hw1 -f /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/input4.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output4_1.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output4_2.txt;
+time /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/code/cmake-build-debug/hw1 -f /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/test04.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/out04_1.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/out04_2.txt;
 
 echo "\nTest #5";
-time /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/code/cmake-build-debug/hw1 -f /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/input5.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output5_1.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output5_2.txt;
+time /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/code/cmake-build-debug/hw1 -f /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/test05.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/out05_1.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/out05_2.txt;
 
 
 echo "\nRandom test #1";
-time /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/code/cmake-build-debug/hw1 -n 100 /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output_random2_1.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output_random2_2.txt;
+time /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/code/cmake-build-debug/hw1 -n 100 /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/outrand01_1.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/outrand01_2.txt;
 
 echo "\nRandom Test #2";
-time /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/code/cmake-build-debug/hw1 -n 10000 /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output_random2_1.txt /Users/mac/Documents/University/computer-system-architecture-hw1/homework/hw1/tests/output_random2_2.txt;
+time /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/code/cmake-build-debug/hw1 -n 10000 /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/outrand02_1.txt /Users/mac/CLionProjects/computer-system-architecture/homework/hw1/tests/outrand02_2.txt;
