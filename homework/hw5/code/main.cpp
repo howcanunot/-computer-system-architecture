8595671bdb4dfaa24ece41b40c63622710ddfe79
howcanunot <kernbldhf@gmail.com>
2021-12-12 20:49:43 +0300

    logic complete

--- homework/hw5/code/main.cpp
+++ homework/hw5/code/main.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
+#include <cstring>
 #include "pin/pin.h"
 #include "people/evaluator.h"
 #include "people/grinder.h"
-#include <random>
+#include "people/controller.h"
 
 
-int main() {
-    Pin pins[10];
-    for (int i = 0; i < 10; i++) {
-        pins[i] = Pin(static_cast<double>(random()) / static_cast<double>(RAND_MAX), random() % 10);
+int main(int argc, char** argv) {
+    if (argc < 2 || argc > 4) {
+        std::cout << "Error!";
+        exit(1);
     }
 
-    Conveyer conveyer;
+    Pin* pins;
+    size_t size = 0;
+    std::string path;
 
-    Evaluator evaluator(&conveyer, pins, 10);
-    Grinder grinder(&conveyer);
+    if (!strcmp("-n", argv[1])) {
+        size = atoi(argv[2]);
+        pins = Pin::GenerateRandomPins(size);
+
+        if (argc == 4) {
+            path = argv[3];
+        }
+
+    } else if (!strcmp("-f", argv[1])) {
+        size = 0;
+        if (argc < 3) {
+            std::cout << "Error";
+            exit(1);
+        }
+
+        std::string path_in(argv[2]);
+        std::ifstream file(path_in);
+
+        pins = Pin::GeneratePinsFromFile(file, size);
+
+        if (argc == 4) {
+            path = argv[3];
+        }
+
+    }
+
+    SynchronizedQueue in;
+    SynchronizedQueue out;
+
+    Evaluator evaluator(&in, pins, size, path);
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    Grinder grinder(&in, &out, path);
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    Controller controller(&out, path);
 
     return 0;
 }
