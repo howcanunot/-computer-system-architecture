40b407795c6a386fd35b034fe7d776e0d337139c
howcanunot <kernbldhf@gmail.com>
2021-12-12 20:48:43 +0300

    add generate pins

--- homework/hw5/code/pin/pin.cpp
+++ homework/hw5/code/pin/pin.cpp
@@ -1,19 +1,25 @@
 #include "pin.h"
 #include <algorithm>
+#include <chrono>
+#include <thread>
 
-Pin::Pin(double curvature, double sharpness) {
+Pin::Pin(double curvature, double sharpness, size_t id) {
     curvature_ = curvature;
     sharpness_ = sharpness;
+    id_ = id;
 }
 
 void Pin::Sharpen() {
     sharpness_ = std::min(10.0, sharpness_ * 1.5);
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 }
 
 double Pin::GetQuality() const {
+    std::this_thread::sleep_for(std::chrono::milliseconds(7500));
     return sharpness_ * curvature_;
 }
 
-bool Pin::CheckCurvature() {
+bool Pin::CheckCurvature() const {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     return curvature_ >= 0.3;
 }
--- homework/hw5/code/pin/pin.h
+++ homework/hw5/code/pin/pin.h
@@ -2,18 +2,21 @@
 #define HW5_PIN_H
 
 #include <iostream>
+#include <fstream>
+#include <random>
 
 class Pin {
 private:
+    size_t id_;
     double curvature_;
     double sharpness_;
 
 public:
     Pin() = default;
 
-    Pin(double curvature, double sharpness);
+    Pin(double, double, size_t);
 
-    bool CheckCurvature();
+    bool CheckCurvature() const;
 
     void Sharpen();
 
@@ -22,6 +25,39 @@ public:
     friend std::ostream& operator<<(std::ostream& os, const Pin& pin) {
         os << "[Pin]: curvature = " << pin.curvature_ << "; sharpness = " << pin.sharpness_;
         return os;
+    }
+
+    static Pin* GenerateRandomPins(size_t size) {
+        Pin* pins = new Pin[size];
+
+        for (size_t i = 0; i < size; i++) {
+            double curvature = static_cast<double>(random()) / static_cast<double>(RAND_MAX);
+            double sharpness = static_cast<double>(random() % 10);
+
+            pins[i] = Pin(curvature, sharpness, i);
+        }
+
+        return pins;
+    }
+
+    static Pin* GeneratePinsFromFile(std::ifstream& in, size_t& size) {
+        if (!in.is_open()) {
+            std::cout << "Error";
+            exit(1);
+        }
+
+        in >> size;
+
+        Pin* pins = new Pin[size];
+
+        for (size_t i = 0; i < size; i++) {
+            double curvature, sharpness;
+            in >> curvature >> sharpness;
+
+            pins[i] = Pin(curvature, sharpness, i);
+        }
+
+        return pins;
     }
 };
 
